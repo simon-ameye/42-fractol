2f12e6fb121cd4cc1d048bb434359d6d8617acd8
simon-ameye <ameye.simon@gmail.com>
2021-09-08 16:23:49 +0200

    norminette OKOK

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 01:53:08 by sameye            #+#    #+#             */
-/*   Updated: 2021/05/21 13:19:03 by sameye           ###   ########.fr       */
+/*   Updated: 2021/09/08 16:23:26 by sameye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int				ft_isprint(int c);
 char			*ft_itoa(int n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memset (void *s, int c, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
